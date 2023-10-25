8a35d191233a43ffba60e502bbff6f4a5682347f
TugasApi <fadhlurahmanseptio@gmail.com>
2023-10-25 08:07:35 +0700

    update cuyy

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="row">
                     <div class="col">
                         <div class="row text-secondary">Welcome,</div>
-                        <div class="row fw-bold" style="font-size: 25px;">
+                        <div class="row fw-bold" style="font-size: 15px;">
                             {{ Auth::user()->name}}
                         </div>
                     </div>
@@ -88,20 +88,20 @@
                     <div class="row">
                         <div class="col">
                             <div class="row text-secondary">Welcome,</div>
-                            <div class="row fw-bold" style="font-size: 25px;">
+                            <div class="row fw-bold" style="font-size: 15px;">
                                 {{ Auth::user()->name}}
                             </div>
                         </div>
                     </div>
-                    <div class="row text-bg-primary p-3" style="height: 170px;">
-                        <div class="col">
-                            <div class="row">
+                    <div class="row">
+                        <div class="col d-flex justify-content-end">
+                            <div class="row"style="font-size:25px;">
                                 <div class="col">Saldo Anda: </div>
                             </div>
-                            <div class="row"style="font-size:50px;">
+                            <div class="row"style="font-size:25px;">
                                 <div class="col">Rp {{ number_format($saldo) }}</div>
                             </div>
-                            <div class="col d-flex justify-content-center align-items-center">
+                            <div class="col d-flex justify-content-start align-items-center">
                                 <!-- Button trigger modal -->
                                 <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wallet" viewBox="0 0 16 16">
@@ -147,7 +147,7 @@
 <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card mb-3">
-                <div class="card-header text-bg-primary fw-bold text-center">Products</div>
+                <div class="card-header text-bg-secondary fw-bold text-center">Products</div>
                 <div class="card-body">
                     <div class="row">
                         @foreach($products as $product)
@@ -175,7 +175,7 @@
                                             </div>
                                             <div class="col-7">
                                                 <div class="d-grip gap-2">
-                                                    <button type="submit" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                                                    <button type="submit" class="btn btn-secondary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
                                                         <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
                                                         <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                                                         </svg> <span style="font-size: 12px;">Tambah</span>
@@ -192,9 +192,9 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
             <div class="card mb-3">
-                <div class="card-header text-bg-primary fw-bold text-center">
+                <div class="card-header text-bg-secondary fw-bold text-center">
                     Baskets
                 </div>
                 <div class="card-body">
@@ -209,12 +209,12 @@
                     <form action="{{ route('payNow')}}" method="POST">
                         <div class="d-grip gap-2">
                             @csrf
-                            <button type="submit" class="btn btn-success">Bayar Sekarang</button>
+                            <button type="submit" class="btn btn-info">Bayar Sekarang</button>
                         </div>
                     </form>
                 </div>
             </div>
-            <div class="card mb-3">
+            <div class="card mb-3 text-bg-secondary">
                 <div class="card-header">
                     Transactions History
                 </div>
@@ -240,8 +240,10 @@
                     @endforeach
                 </div>
             </div>
+            <divclass="row justify-content-center">
+            <div class="col-md-8">
             <div class="card mb-3">
-                <div class="card-header text-center fw-bold text-bg-primary">
+                <div class="card-header text-center fw-bold text-bg-secondary">
                     Mutasi Wallet
                 </div>
                 @foreach($mutasi as $data)
@@ -257,6 +259,8 @@
                     </div>
                 </div>
                 @endforeach
+            </div>
+            </div>
             </div>
         </div>
     </div>
